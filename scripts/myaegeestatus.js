d1e8b9026157fe38d54e47b0f04be3d645a7c465
linuxbandit <bellicaf@tcd.ie>
2020-09-18 08:50:18 +0300

    docs: add documentation

--- scripts/myaegeestatus.js
+++ scripts/myaegeestatus.js
@@ -1,23 +1,31 @@
 // Description:
 //   Query services status - simple reading of my.aegee.eu/status
+//   It also provides the version deployed and the latest released version from github
 //
 //   Examples:
 //   - `hubot what status is frontend` - Get status of frontend
 //   - `hubot what version is frontend` - Get version of frontend
+//   - `hubot which is last frontend` - Get latest released version of frontend
 //   - `hubot what status is core` - Get status of core
 //   - `hubot what version is core` - Get version of core
+//   - `hubot which is last core` - Get latest released version of core
 //   - `hubot what status is events` - Get status of events
 //   - `hubot what version is events` - Get version of events
+//   - `hubot which is last events` - Get latest released version of events
 //   - `hubot what status is statutory` - Get status of statutory
 //   - `hubot what version is statutory` - Get version of statutory
+//   - `hubot which is last statutory` - Get latest released version of statutory
 //   - `hubot what status is discounts` - Get status of discounts
 //   - `hubot what version is discounts` - Get version of discounts
+//   - `hubot which is last discounts` - Get latest released version of discounts
 //   - `hubot what status is mailer` - Get status of mailer
 //   - `hubot what version is mailer` - Get version of mailer
+//   - `hubot which is last mailer` - Get latest released version of mailer
 //
 // Commands:
 //   hubot what status is (frontend|core|events|statutory|discounts|mailer)
 //   hubot what version is (frontend|core|events|statutory|discounts|mailer)
+//   hubot which is last (frontend|core|events|statutory|discounts|mailer)
 //
 // Author:
 //   https://github.com/linuxbandit
